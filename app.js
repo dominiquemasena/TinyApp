85d7593be988cdf61451a9c30421eaafcda08254
Dominique Masena <dominique.masena@live.ca>
2020-08-03 05:41:45 +0000

    app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 const requestHandler = function(request, response) {
+  console.log('In requestHandler');
   response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
 };
 
 const server = http.createServer(requestHandler);
+console.log('Server created');
 
 server.listen(PORT, () => {
   console.log(`Server listening on: http://localhost:${PORT}`);
 });
+
+console.log('Last line (after .listen call)');
