032f796c2c8daf29758132ec0994a426c2750312
Dominique Masena <dominique.masena@live.ca>
2020-08-03 06:20:52 +0000

    express_server.js

--- express_server.js
+++ express_server.js
@@ -1,53 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-
+  "9sm5xK": "http://www.google.com"
 };
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls", (req, res) => {
-  let webPage = { urls: urlDatabase };
-  res.render("urls_index", webPage);
-});
-
-app.get("/urls:shortURL", (req, res) => {
-  let webPage = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", webPage);
-});
-
-app.get('_header', function(req, res) {
-  res.render('partials/_header');
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
