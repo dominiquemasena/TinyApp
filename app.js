386f205d3bc30c509033d9ca3e9635993280f2ec
Dominique Masena <dominique.masena@live.ca>
2020-08-03 05:38:25 +0000

    app.js

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 const requestHandler = function(request, response) {
-  if (request.url === "/") {
-    response.end("Welcome!");
-  } else if (request.url === "/urls") {
-    response.end("www.lighthouselabs.ca\nwww.google.com");
-  } else {
-    response.statusCode = 404;
-    response.end("404 Page Not Found");
-  }
+  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
 };
+
 const server = http.createServer(requestHandler);
 
 server.listen(PORT, () => {
