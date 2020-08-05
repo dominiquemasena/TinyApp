89afbe26ea67ca5cdc040ba64c9d6cde2b261ff7
Dominique Masena <dominique.masena@live.ca>
2020-08-05 16:30:00 +0000

    express_server.js

--- express_server.js
+++ express_server.js
@@ -9,8 +9,6 @@ function generateRandomString() {
   return Math.random().toString(36).substring(2,8);
 }
 
-
-
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -21,10 +19,15 @@ const urlDatabase = {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-
 });
 
-
+app.get("/urls/:shortURL/edit", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
@@ -37,30 +40,45 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+
 app.post('/urls/:short/delete', (req,res) => {
   delete urlDatabase[req.params.short]
   res.redirect(`/urls/`);
 })
 
-// app.post('/urls/:short/edit', (req,res) => {
-//   edit urlDatabase[req.params.short]
-//   res.redirect(`/urls/`);
-// })
+//* */
+app.post("/urls/:shortURL/edit", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls")
+}); 
+//* */
+
+
+// app.post("/urls/:shortURL", (req, res) => {
+//   let shortURL = req.params.shortURL;
+//   let longURL = req.body.longURL;
+//   urlDatabase[shortURL] = longURL;
+//   res.redirect("/urls")
+// });
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase[shortURL])
   res.redirect(`/urls/${shortURL}`)
 })
 //generating random string 
 
 app.post("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase.shortURL;
+  console.log(urlDatabase[shortURL])
   res.redirect(longURL);
 
 })
 //req.params 
-
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -74,12 +92,35 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 })
 
-
-
 // urlDatabase.insert()
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 
+// app.post('/urls/:short/edit', (req,res) => {
+//   let shortURL = req.params.shortURL;
+  
+//   //  urlDatabase[req.params.short]
+//   res.redirect(`/urls/`);
+// })
 
+
+{/* <form method="POST" action="/urls/<%=url%>/delete">
+                <button type="submit" class="btn btn-primary">Delete</button>    
+              </form> */}
+
+// app.post('/urls/:short/edit', (req, res) => {
+//   let shortURL = req.params.shortURL;
+//   let longURL = req.body.longURL;
+//   urlDatabase[shortURL] = longURL;
+//   res.redirect(`/urls/`)
+// });
+
+// app.post('/urls/:short/edit', (req,res) => {
+//   const longURL = urlDatabase[req.params.short];
+//   res.redirect(`/urls/`);
+// })
+{/* <form method="POST" action="/urls/<%=url%>/edit">
+<button type="submit" class="btn btn-primary">Edit</button>    
+</form> */}
